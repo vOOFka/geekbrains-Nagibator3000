af3bd2cc4c81284b1ea616ecf249dd13a8dcf338
Constantin <k.kamenchukov@gmail.com>
2022-07-29 19:38:45 +0300

    Update

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/AnimationViewController.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/AnimationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AnimationViewController.swift
 //  geekbrains-Nagibator3000
 //
 //  Created by Константин Каменчуков on 29.07.2022.
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class AnimationViewController: UIViewController {
+    
+    
+    private var current: UIViewController
+    init() {
+        self.current = MainTabBarViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .blue
+        makeServiceCall()
         
     }
 
-
+    private func makeServiceCall() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(3)) {
+            self.transitionToMain()
+        }
+       
+    }
+  
+   private func transitionToMain() {
+        addChild(current)
+        current.view.frame = view.bounds
+        view.addSubview(current.view)
+        current.didMove(toParent: self)
+    }
 }
 
--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/MainTabBarViewController.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/MainTabBarViewController.swift
@@ -5,4 +5,17 @@
 //  Created by Константин Каменчуков on 29.07.2022.
 //
 
-import Foundation
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: (77 / 255), green: (175 / 255), blue: (80 / 255), alpha: 1)
+        
+    }
+    
+    
+}
+
