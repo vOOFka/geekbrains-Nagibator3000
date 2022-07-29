a93802f481c67679c004c01344e97518799cbb37
Constantin <k.kamenchukov@gmail.com>
2022-07-29 17:18:11 +0300

    Created Welcome Screen and Transition to Main

---
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/AnimationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geekbrains-Nagibator3000
+//
+//  Created by Константин Каменчуков on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/MainTabBarViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabBarViewController.swift
+//  geekbrains-Nagibator3000
+//
+//  Created by Константин Каменчуков on 29.07.2022.
+//
+
+import Foundation
