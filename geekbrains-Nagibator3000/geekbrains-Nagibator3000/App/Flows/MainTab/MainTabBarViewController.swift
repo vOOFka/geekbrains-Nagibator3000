029f43985debd93d8abeb9d482e808a37a81716c
Vladimir Sirel <exit551@ya.ru>
2022-07-31 23:58:19 +0300

    Added VCs to MainTabVC

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/MainTab/MainTabBarViewController.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/MainTab/MainTabBarViewController.swift
@@ -1,8 +1,21 @@
 import UIKit
 
-class MainTabBarViewController: UITabBarController {
+final class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tabBar.isTranslucent = true
+        tabBar.backgroundColor = ColorScheme.greenPantone.color
+        tabBar.tintColor = ColorScheme.white.color
+        
+        let translateViewController = TranslateViewController()
+        let dictionaryViewController = DictionaryViewController()
+        let trainingViewController = TrainingViewController()
+        
+        translateViewController.title = String(localized: "Translate")
+        dictionaryViewController.title = String(localized: "Dictionary")
+        trainingViewController.title = String(localized: "Training")
+        
+        setViewControllers([translateViewController, dictionaryViewController, trainingViewController], animated: false)
     } 
 }
-
