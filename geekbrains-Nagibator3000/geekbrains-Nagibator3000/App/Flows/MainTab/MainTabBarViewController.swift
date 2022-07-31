b555e2b2af09ad633a085d8fee90ea1493ef53c6
Vladimir Sirel <exit551@ya.ru>
2022-08-01 00:22:13 +0300

    Added images for tabs in MainTabBarViewController, refactoring

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/MainTab/MainTabBarViewController.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/MainTab/MainTabBarViewController.swift
@@ -3,19 +3,33 @@ import UIKit
 final class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tabBar.isTranslucent = true
+        initialConfig()
+    }
+    
+    //MARK: - Config
+    
+    private func initialConfig() {
         tabBar.backgroundColor = ColorScheme.greenPantone.color
         tabBar.tintColor = ColorScheme.white.color
         
-        let translateViewController = TranslateViewController()
-        let dictionaryViewController = DictionaryViewController()
-        let trainingViewController = TrainingViewController()
+        let translateViewController = UINavigationController(rootViewController: TranslateViewController())
+        let dictionaryViewController = UINavigationController(rootViewController: DictionaryViewController())
+        let trainingViewController = UINavigationController(rootViewController: TrainingViewController())
         
         translateViewController.title = String(localized: "Translate")
         dictionaryViewController.title = String(localized: "Dictionary")
         trainingViewController.title = String(localized: "Training")
         
         setViewControllers([translateViewController, dictionaryViewController, trainingViewController], animated: false)
-    } 
+        
+        guard let items = tabBar.items else {
+            return
+        }
+        
+        let icons = ["doc.text.magnifyingglass","book.closed","rectangle.3.group.fill"]
+        
+        items.enumerated().forEach { index, item in
+            item.image = UIImage(systemName: icons[index])
+        }
+    }
 }
