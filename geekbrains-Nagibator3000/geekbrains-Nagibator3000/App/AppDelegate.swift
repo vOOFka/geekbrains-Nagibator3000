6331d2e65efdc0ec89942ba8b344ca4c33a991a6
Vladimir Sirel <exit551@ya.ru>
2022-08-02 22:41:15 +0300

    Added custom nav bar appearances

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/AppDelegate.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/AppDelegate.swift
@@ -10,7 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let newNavBarAppearance = customNavBarAppearance()
+            
+        let appearance = UINavigationBar.appearance()
+        appearance.scrollEdgeAppearance = newNavBarAppearance
+        appearance.compactAppearance = newNavBarAppearance
+        appearance.standardAppearance = newNavBarAppearance
+        appearance.compactScrollEdgeAppearance = newNavBarAppearance
+
         return true
     }
 
