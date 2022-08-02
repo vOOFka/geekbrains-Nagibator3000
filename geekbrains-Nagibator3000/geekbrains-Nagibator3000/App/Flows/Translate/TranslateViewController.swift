2b8ea0081e1c3c23fc0573484f0154b0ee9e581f
Vladimir Sirel <exit551@ya.ru>
2022-08-02 22:43:22 +0300

    Added buttons for select language

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Translate/TranslateViewController.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Translate/TranslateViewController.swift
@@ -6,11 +6,74 @@
 //
 
 import UIKit
+import PinLayout
 
 final class TranslateViewController: UIViewController {
+    var viewModel: TranslateViewModel
+    
+    private let buttonSourceLanguage = UIButton()
+    private let buttonDestinationLanguage = UIButton()
+    
+    // MARK: - Init
+
+    init(viewModel: TranslateViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = ColorScheme.fuchsiaBlue.color
-        title = String(localized: "Translate")
+        initialConfig()
     }
+    
+    //MARK: - Config
+    
+    private func initialConfig() {
+        navigationController?.navigationBar.standardAppearance
+        view.backgroundColor = Constants.backgroundColor
+        title = Constants.title
+        
+        buttonSourceLanguage.setTitle(viewModel.sourceLanguage?.code, for: .normal)
+        buttonDestinationLanguage.setTitle(viewModel.destinationLanguage?.code, for: .normal)
+        
+        buttonSourceLanguage.addTarget(self, action:#selector(buttonSelectLanguageTap), for: .touchUpInside)
+        buttonDestinationLanguage.addTarget(self, action:#selector(buttonSelectLanguageTap), for: .touchUpInside)
+        
+        view.addSubview(buttonSourceLanguage)
+        view.addSubview(buttonDestinationLanguage)
+    }
+    
+    //MARK: - Layout
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let navigationBar = navigationController?.navigationBar else {
+            return
+        }
+        
+        view.pin.all()
+        buttonSourceLanguage.pin.below(of: navigationBar).left().width(UIScreen.main.bounds.width / 2).height(60.0)
+        buttonDestinationLanguage.pin.below(of: navigationBar).right().width(UIScreen.main.bounds.width / 2).height(60.0)
+    }
+    
+    // MARK: - Button actions
+    @objc private func buttonSelectLanguageTap(sender : UIButton) {
+        print("+++++++++++++")
+        //private let selectLanguageViewController: UIViewController? = nil
+    }
+}
+
+//MARK: - Constants
+
+private enum Constants {
+    static let title = "Translate".localized
+    
+    static let backgroundColor = ColorScheme.fuchsiaBlue.color
+    static let whiteColor = ColorScheme.white.color
+    static let greenColor = ColorScheme.greenPantone.color
 }
