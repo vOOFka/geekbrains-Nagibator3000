6b4c8364aab59a356cb669ce5a047d4505f532fe
Vladimir Sirel <exit551@ya.ru>
2022-08-02 22:40:30 +0300

    Added Language struct, UIRender extension

---
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Entity/Language.swift
@@ -0,0 +1,16 @@
+//
+//  Language.swift
+//  geekbrains-Nagibator3000
+//
+//  Created by Home on 02.08.2022.
+//
+
+import Foundation
+
+struct Language: Codable {
+    var id: String?
+    var code: String?
+    var name: String?
+    var nativeName: String?
+    var icon: Data?
+}
