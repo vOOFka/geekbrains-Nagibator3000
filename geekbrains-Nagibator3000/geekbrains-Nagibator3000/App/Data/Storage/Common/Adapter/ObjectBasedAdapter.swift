bb4e47812253b1242ed075c21b525b236fe8caf9
Vladimir Sirel <exit551@ya.ru>
2022-08-13 18:55:47 +0300

    Fixed formating

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Data/Storage/Common/Adapter/ObjectBasedAdapter.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Data/Storage/Common/Adapter/ObjectBasedAdapter.swift
@@ -9,56 +9,56 @@ import Foundation
 import RxSwift
 
 class ObjectBasedAdapter<RepositoryModel, StorageModel: Codable> {
-  private let dataStorage: DataStorage
-  private let mapper: Mapper<RepositoryModel, StorageModel>
-
-  init(
-    dataStorage: DataStorage,
-    mapper: Mapper<RepositoryModel, StorageModel>
-  ) {
-    self.dataStorage = dataStorage
-    self.mapper = mapper
-  }
-
-  func read() -> Observable<[RepositoryModel]> {
-    Observable<[RepositoryModel]>.create { [weak self] observable in
-      self?.dataStorage.read { (storageModel: StorageModel?) in
-        guard let storageModel = storageModel as? [StorageModel],
-              let repositoryModel = self?.mapper.mapFromStorage(model: storageModel) else {
-          observable.onError(OtherError())
-          return
+    private let dataStorage: DataStorage
+    private let mapper: Mapper<RepositoryModel, StorageModel>
+    
+    init(
+        dataStorage: DataStorage,
+        mapper: Mapper<RepositoryModel, StorageModel>
+    ) {
+        self.dataStorage = dataStorage
+        self.mapper = mapper
+    }
+    
+    func read() -> Observable<[RepositoryModel]> {
+        Observable<[RepositoryModel]>.create { [weak self] observable in
+            self?.dataStorage.read { (storageModel: StorageModel?) in
+                guard let storageModel = storageModel as? [StorageModel],
+                      let repositoryModel = self?.mapper.mapFromStorage(model: storageModel) else {
+                    observable.onError(OtherError())
+                    return
+                }
+                
+                observable.onNext(repositoryModel)
+                observable.onCompleted()
+            }
+            
+            return Disposables.create()
         }
-        
-        observable.onNext(repositoryModel)
-        observable.onCompleted()
-      }
-      
-      return Disposables.create()
     }
-  }
-
-  func write(model: Observable<[RepositoryModel]>) -> Observable<Bool> {
-    Observable<Bool>.create { [weak self] observable in
-      model
-        .bind { repositoryModel in
-          let storageModel = self?.mapper.mapFromRepository(model: repositoryModel)
-          self?.dataStorage.write(object: storageModel) { comleted in
-            observable.onNext(comleted)
-            observable.onCompleted()
-          }
+    
+    func write(model: Observable<[RepositoryModel]>) -> Observable<Bool> {
+        Observable<Bool>.create { [weak self] observable in
+            let _ = model
+                .bind { repositoryModel in
+                    let storageModel = self?.mapper.mapFromRepository(model: repositoryModel)
+                    self?.dataStorage.write(object: storageModel) { comleted in
+                        observable.onNext(comleted)
+                        observable.onCompleted()
+                    }
+                }
+            
+            return Disposables.create()
         }
-
-      return Disposables.create()
     }
-  }
-
-  func clear() -> Completable {
-    Completable.create { [weak self] completable in
-      self?.dataStorage.clear { _ in
-        completable(.completed)
-      }
-
-      return Disposables.create()
+    
+    func clear() -> Completable {
+        Completable.create { [weak self] completable in
+            self?.dataStorage.clear { _ in
+                completable(.completed)
+            }
+            
+            return Disposables.create()
+        }
     }
-  }
 }
