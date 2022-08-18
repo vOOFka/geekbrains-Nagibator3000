ae1e1fd64cec8758e8620b70ddc7dcb18caf0768
Valerii <valeronmaestro@gmail.com>
2022-08-19 00:50:57 +0300

    refactored traslate screen

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Translate/Screens/Translate/Views/TranslateViewController.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Translate/Screens/Translate/Views/TranslateViewController.swift
@@ -10,161 +10,150 @@ import RxCocoa
 import RxSwift
 
 final class TranslateViewController: UIViewController {
-    var viewModel: TranslateViewModel!
-    private let disposeBag = DisposeBag()
-    
-    private let sourceLanguageButton = UIButton()
-    private let swapLanguageButton = UIButton()
-    private let destinationLanguageButton = UIButton()
-    
-    private let sourceTextView = CustomTextView()
-    private let destinationTextView = CustomTextView()
-    private let actionView = CustomActionsView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        initialConfig()
-        bindViewModel()
-    }
+  var viewModel: TranslateViewModel!
+  private let disposeBag = DisposeBag()
   
-    override func viewWillAppear(_ animated: Bool) {
-      super.viewWillAppear(animated)
-      tabBarController?.navigationItem.setupTitle(text: Constants.title)
-    }
+  private let sourceLanguageButton = UIButton()
+  private let swapLanguageButton = UIButton()
+  private let destinationLanguageButton = UIButton()
   
-    //MARK: - Config
-    
-    private func initialConfig() {
-        view.backgroundColor = Constants.backgroundColor
-        sourceTextView.delegate = self
-        destinationTextView.isEditable = false
-        
-        configLanguageButtons()
-        actionView.delegate = self
-        
-        view.addSubview(sourceLanguageButton)
-        view.addSubview(destinationLanguageButton)
-        view.addSubview(swapLanguageButton)
-        
-        view.addSubview(sourceTextView)
-        view.addSubview(destinationTextView)
-        
-        view.addSubview(actionView)
+  private let sourceTextView = CustomTextView()
+  private let destinationTextView = CustomTextView()
+  private let actionView = CustomActionsView()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    initialConfig()
+    bindViewModel()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    tabBarController?.navigationItem.setupTitle(text: Constants.title)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    guard let navigationBar = navigationController?.navigationBar else {
+      return
     }
+    let mainWidth = UIScreen.main.bounds.width
     
-    private func configLanguageButtons() {
-        swapLanguageButton.setImage(Constants.swapIcon, for: .normal)
-        swapLanguageButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        
-        var configuration = UIButton.Configuration.plain()
-        configuration.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 30.0)
-        swapLanguageButton.configuration = configuration
-        
-        swapLanguageButton.addTarget(self, action:#selector(swapLanguageButtonTap), for: .touchUpInside)
-        
-        sourceLanguageButton.addTarget(self, action:#selector(selectLanguageButtonTap), for: .touchUpInside)
-        destinationLanguageButton.addTarget(self, action:#selector(selectLanguageButtonTap), for: .touchUpInside)
-        
-        sourceLanguageButton.accessibilityLabel = "source"
-        destinationLanguageButton.accessibilityLabel = "destination"
-    }
+    let buttonHeight: CGFloat = 60.0
+    let buttonWidth: CGFloat = (mainWidth / 2) - buttonHeight / 2
     
-    private func bindViewModel() {
-        viewModel.sourceLanguage.asObservable().bind { [weak self] value in
-          self?.sourceLanguageButton.setTitle(value.code?.uppercased(), for: .normal)
-        }.disposed(by: disposeBag)
-      
-        viewModel.destinationLanguage.asObservable().bind { [weak self] value in
-            self?.destinationLanguageButton.setTitle(value.code?.uppercased(), for: .normal)
-        }.disposed(by: disposeBag)
-        
-        viewModel.translatedText.asObservable().bind { [weak self] text in
-            self?.destinationTextView.text = text
-        }.disposed(by: disposeBag)
-    }
+    view.pin.all()
+    sourceLanguageButton.pin.below(of: navigationBar).left().width(buttonWidth).height(buttonHeight)
+    destinationLanguageButton.pin.below(of: navigationBar).right().width(buttonWidth).height(buttonHeight)
+    swapLanguageButton.pin.horizontallyBetween(sourceLanguageButton, and: destinationLanguageButton, aligned: .center).height(buttonHeight)
     
-    //MARK: - Layout
+    sourceTextView.pin.below(of: swapLanguageButton, aligned: .center).width(mainWidth - 40.0).height(mainWidth / 3).margin(20.0)
+    destinationTextView.pin.below(of: sourceTextView, aligned: .center).width(mainWidth - 40.0).height(mainWidth / 3).margin(20.0)
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        guard let navigationBar = navigationController?.navigationBar else {
-            return
-        }
-        let mainWidth = UIScreen.main.bounds.width
-        
-        let buttonHeight: CGFloat = 60.0
-        let buttonWidth: CGFloat = (mainWidth / 2) - buttonHeight / 2
-        
-        view.pin.all()
-        sourceLanguageButton.pin.below(of: navigationBar).left().width(buttonWidth).height(buttonHeight)
-        destinationLanguageButton.pin.below(of: navigationBar).right().width(buttonWidth).height(buttonHeight)
-        swapLanguageButton.pin.horizontallyBetween(sourceLanguageButton, and: destinationLanguageButton, aligned: .center).height(buttonHeight)
-        
-        sourceTextView.pin.below(of: swapLanguageButton, aligned: .center).width(mainWidth - 40.0).height(mainWidth / 3).margin(20.0)
-        destinationTextView.pin.below(of: sourceTextView, aligned: .center).width(mainWidth - 40.0).height(mainWidth / 3).margin(20.0)
-        
-        actionView.pin.below(of: destinationTextView, aligned: .right).width(mainWidth).height(buttonHeight * 0.6).marginTop(16.0)
-    }
+    actionView.pin.below(of: destinationTextView, aligned: .right).width(mainWidth).height(buttonHeight * 0.6).marginTop(16.0)
+  }
+  
+  private func initialConfig() {
+    view.backgroundColor = Constants.backgroundColor
+    setupTextViews()
+    setupButtons()
+    setupActionView()
+  }
+  
+  private func setupTextViews() {
+    sourceTextView.delegate = self
+    destinationTextView.isEditable = false
     
-    // MARK: - Buttons actions
-    @objc private func selectLanguageButtonTap(sender : UIButton) {
-        let type: LanguageType = (sender.accessibilityLabel == "source") ? .source : .destination
-        let selectLanguageViewController = SelectLanguageViewController(viewModel: viewModel, type: type)
-        
-        let navigationController = UINavigationController(rootViewController: selectLanguageViewController)
-        navigationController.isNavigationBarHidden = false
-        navigationController.modalPresentationStyle = .automatic
-        
-        present(navigationController, animated: true)
-    }
+    view.addSubview(sourceTextView)
+    view.addSubview(destinationTextView)
+  }
+  
+  private func setupButtons() {
+    swapLanguageButton.setImage(Constants.swapIcon, for: .normal)
+    swapLanguageButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+    var configuration = UIButton.Configuration.plain()
+    configuration.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 30.0)
+    swapLanguageButton.configuration = configuration
+
+    view.addSubview(sourceLanguageButton)
+    view.addSubview(destinationLanguageButton)
+    view.addSubview(swapLanguageButton)
+  }
+  
+  private func setupActionView() {
+    actionView.delegate = self
+    view.addSubview(actionView)
+  }
+  
+  private func bindViewModel() {
+    swapLanguageButton.rx.tap
+      .map { _ in Void() }
+      .bind(to: viewModel.input.onReverseLanguage)
+      .disposed(by: disposeBag)
     
-    @objc private func swapLanguageButtonTap(sender : UIButton) {
-        viewModel.swapLanguages()
-    }
+    sourceLanguageButton.rx.tap
+      .map { _ in Void() }
+      .bind(to: viewModel.input.onTapFromLanguage)
+      .disposed(by: disposeBag)
+    
+    destinationLanguageButton.rx.tap
+      .map { _ in Void() }
+      .bind(to: viewModel.input.onTapToLanguage)
+      .disposed(by: disposeBag)
+    
+    viewModel.output.translateText
+      .map { $0.toText }
+      .drive(destinationTextView.rx.text)
+      .disposed(by: disposeBag)
+    
+    viewModel.output.destinationLanguage
+      .map { $0.name.capitalized }
+      .drive(destinationLanguageButton.rx.title())
+      .disposed(by: disposeBag)
+    
+    viewModel.output.sourceLanguage
+      .map { $0.name.capitalized }
+      .drive(sourceLanguageButton.rx.title())
+      .disposed(by: disposeBag)
+  }
 }
 
 extension TranslateViewController: CustomActionsDelegate {
-    func someActionButtonTap(sender: UIButton) {
-        guard let label = sender.accessibilityLabel,
-              let senderType = ActionButtonType.actionType(from: label) else {
-            return
-        }
-        switch senderType {
-        case .shareButton:
-            shareButtonTap(text: destinationTextView.text)
-        case .copyButton:
-            UIPasteboard.general.string = destinationTextView.text
-        case .saveButton:
-            viewModel?.saveToDictionary(fromText: sourceTextView.text, toText: destinationTextView.text)
-        }
+  func someActionButtonTap(sender: UIButton) {
+    guard let label = sender.accessibilityLabel,
+          let senderType = ActionButtonType.actionType(from: label) else {
+      return
     }
-    
-    func shareButtonTap(text: String) {
-        let objectsToShare = [text]
-        let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
-        activityVC.excludedActivityTypes = [.airDrop, .addToReadingList]
-        self.present(activityVC, animated: true, completion: nil)        
+    switch senderType {
+    case .shareButton:
+      viewModel.input.onShare.accept(Void())
+    case .copyButton:
+      UIPasteboard.general.string = destinationTextView.text
+      viewModel.input.onCopy.accept(Void())
+    case .saveButton:
+      viewModel.input.onSave.accept(Void())
     }
+  }
 }
 
 extension TranslateViewController: UITextViewDelegate {
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
-            viewModel.translate(text: textView.text)
-            textView.resignFirstResponder()
-        }
-        return true
+  func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    if text == "\n" {
+      viewModel.input.onTranslate.accept(text)
+      textView.resignFirstResponder()
     }
+    return true
+  }
 }
 
 //MARK: - Constants
 
 private enum Constants {
-    static let title = "Translate".localized
-    
-    static let swapIcon = UIImage(systemName: "arrow.left.and.right.square")?.withTintColor(whiteColor, renderingMode: .alwaysOriginal)
-    
-    static let backgroundColor = ColorScheme.greenPantone.color
-    static let whiteColor = ColorScheme.white.color
-    static let greenColor = ColorScheme.greenPantone.color
+  static let title = "Translate".localized
+  
+  static let swapIcon = UIImage(systemName: "arrow.left.and.right.square")?.withTintColor(whiteColor, renderingMode: .alwaysOriginal)
+  
+  static let backgroundColor = ColorScheme.greenPantone.color
+  static let whiteColor = ColorScheme.white.color
+  static let greenColor = ColorScheme.greenPantone.color
 }
