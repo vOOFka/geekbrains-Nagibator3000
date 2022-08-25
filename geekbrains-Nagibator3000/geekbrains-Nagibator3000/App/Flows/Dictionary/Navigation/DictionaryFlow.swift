1d4a864abfe8d2b7bf93d200d8f22c6b23519e97
Valerii <valeronmaestro@gmail.com>
2022-08-25 21:17:30 +0300

    open first screen

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Dictionary/Navigation/DictionaryFlow.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Dictionary/Navigation/DictionaryFlow.swift
@@ -42,20 +42,6 @@ class DictionaryFlow: Flow {
   }
     
     private func openTranslateScreen() -> FlowContributors {
-        let translateViewModel = TranslateViewModel(
-            translaterUseCase: container.resolve(TranslaterUseCase.self)!,
-            dictionaryUseCase: container.resolve(DictionaryUseCase.self)!
-        )
-        let translateViewController = TranslateViewController()
-        translateViewController.viewModel = translateViewModel
-        
-        if let tabBarViewController = self.viewController.tabBarController,
-           let translateViewController = tabBarViewController.viewControllers?.compactMap({ $0 as? TranslateViewController }).first {
-            tabBarViewController.selectedViewController = translateViewController
-        }
-        
-        return .one(flowContributor: .contribute(
-            withNextPresentable: translateViewController, withNextStepper: translateViewModel)
-        )
+      return .end(forwardToParentFlowWithStep: MainStep.goToApp)
     }
 }
