16ce64ab1c8d0793745b3a6e22ebec93c0a2c9d6
Valerii <valeronmaestro@gmail.com>
2022-08-28 20:12:54 +0300

    finished 3th screen

--- geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Training/Screens/Views/TrainingViewController.swift
+++ geekbrains-Nagibator3000/geekbrains-Nagibator3000/App/Flows/Training/Screens/Views/TrainingViewController.swift
@@ -13,15 +13,14 @@ import Lottie
 import RxFlow
 
 final class TrainingViewController: UIViewController, Stepper {
-  private var loadingAnimationView: AnimationView!
   private var cupAnimationView: AnimationView!
   
   private var emptyHolderView = UIView()
   private var addButton = UIButton()
   private var emptyLabel = UILabel()
-    
+  
   var steps = PublishRelay<Step>()
-    
+  
   var viewModel: TrainingViewModel!
   var kolodaView = KolodaView(frame: CGRect(x: 0, y: 0, width: 250, height: 400))
   
@@ -32,25 +31,20 @@ final class TrainingViewController: UIViewController, Stepper {
     super.viewDidLoad()
     view.backgroundColor = ColorScheme.white.color
     view.addSubview(kolodaView)
-    kolodaView.dataSource = self
     kolodaView.delegate = self
+    kolodaView.dataSource = self
     bindLifeCycle()
     bindSource()
-      setupLoadingAnimationView()
-      setupCupAnimationView()
-      configEmptyHolderView()
-      bindStates()
+    setupCupAnimationView()
+    configEmptyHolderView()
+    bindStates()
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
+    kolodaView.pin.center()
     tabBarController?.navigationItem.setupTitle(text: Constants.title)
     tabBarController?.navigationItem.rightBarButtonItems?.removeAll()
-  }
-  
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-    kolodaView.pin.center()
   }
   
   private func bindLifeCycle() {
@@ -64,38 +58,43 @@ final class TrainingViewController: UIViewController, Stepper {
       .drive { [weak self] source in
         self?.trainingCards = source
           .compactMap{ model in TrainingCardView(with: model.fromText, translate: model.toText) }
-        self?.kolodaView.reloadData()
+        self?.kolodaView.resetCurrentCardIndex()
       }
       .disposed(by: disposeBag)
   }
-    private func bindStates() {
-        viewModel.output.state
-        .drive { [weak self] state in
-            switch state {
-            case .load:
-                self?.kolodaView.isHidden = true
-                self?.loadingAnimationView.isHidden = false
-                self?.loadingAnimationView.pin.center()
-                self?.loadingAnimationView.play()
-            case .empty:
-                self?.loadingAnimationView.isHidden = true
-                self?.loadingAnimationView.stop()
-                self?.emptyHolderView.isHidden = false
-                self?.kolodaView.isHidden = true
-            case .completed:
-                self?.loadingAnimationView.isHidden = true
-                self?.loadingAnimationView.stop()
-                self?.kolodaView.isHidden = false
-            }
+  
+  private func bindStates() {
+    viewModel.output.state
+      .drive { [weak self] state in
+        switch state {
+        case .load:
+          self?.kolodaView.isHidden = false
+          self?.emptyHolderView.isHidden = true
+          self?.cupAnimationView.isHidden = true
+          
+        case .empty:
+          self?.emptyHolderView.isHidden = false
+          self?.kolodaView.isHidden = true
+          self?.cupAnimationView.isHidden = true
+          
+        case .completed:
+          self?.emptyHolderView.isHidden = true
+          self?.cupAnimationView.isHidden = true
+          self?.kolodaView.isHidden = false
         }
-        .disposed(by: disposeBag)
-    }
-    
+      }
+      .disposed(by: disposeBag)
+  }
+  
 }
 
 extension TrainingViewController: KolodaViewDelegate {
   func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
-    kolodaView.reloadData()
+    kolodaView.isHidden = true
+    emptyHolderView.isHidden = true
+    cupAnimationView.pin.center()
+    cupAnimationView.isHidden = false
+    cupAnimationView.play()
   }
 }
 
@@ -111,77 +110,79 @@ extension TrainingViewController: KolodaViewDataSource {
   func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
     return .default
   }
+  
   func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
     return TrainingOverlayView()
+  }
+  
+  func koloda(_ koloda: KolodaView, didSwipeCardAt index: Int, in direction: SwipeResultDirection) {
+    guard direction == .right || direction == .topRight || direction == .bottomRight else { return }
+    
+    viewModel.input.delete.accept(index)
   }
 }
 
 // MARK: - Empty holder view
 
 extension TrainingViewController {
-    private func configEmptyHolderView() {
-        emptyLabel.text = Constants.emptyLabelText
-        emptyLabel.font = Constants.boldFont
-        emptyLabel.numberOfLines = 0
-        emptyLabel.textAlignment = .center
-        
-        addButton.setTitle(Constants.titleAddButton, for: .normal)
-        addButton.backgroundColor = Constants.blueColor
-        addButton.setTitleColor(Constants.whiteColor, for: .normal)
-        addButton.layer.cornerRadius = 6.0
-        addButton.clipsToBounds = true
-        addButton.addTarget(self, action:#selector(addButtonTap), for: .touchUpInside)
-        
-        emptyHolderView.addSubview(emptyLabel)
-        emptyHolderView.addSubview(addButton)
-        view.addSubview(emptyHolderView)
-        
-        emptyHolderView.pin.all()
-        emptyLabel.pin.vCenter(-50.0).horizontally(20.0).sizeToFit(.width)
-        addButton.pin.below(of: emptyLabel, aligned: .center).height(44.0).width(emptyLabel.frame.width * 0.7).margin(20.0)
-        
-        emptyHolderView.isHidden = true
-    }
+  private func configEmptyHolderView() {
+    emptyLabel.text = Constants.emptyLabelText
+    emptyLabel.font = Constants.boldFont
+    emptyLabel.numberOfLines = 0
+    emptyLabel.textAlignment = .center
     
-    @objc private func addButtonTap(sender: UIButton) {
-        viewModel.input.onAdd.accept(Void())
-    }
+    addButton.setTitle(Constants.titleAddButton, for: .normal)
+    addButton.backgroundColor = Constants.blueColor
+    addButton.setTitleColor(Constants.whiteColor, for: .normal)
+    addButton.layer.cornerRadius = 6.0
+    addButton.clipsToBounds = true
+    addButton.addTarget(self, action:#selector(addButtonTap), for: .touchUpInside)
+    
+    emptyHolderView.addSubview(emptyLabel)
+    emptyHolderView.addSubview(addButton)
+    view.addSubview(emptyHolderView)
+    
+    emptyHolderView.pin.all()
+    emptyLabel.pin.vCenter(-50.0).horizontally(20.0).sizeToFit(.width)
+    addButton.pin.below(of: emptyLabel, aligned: .center).height(44.0).width(emptyLabel.frame.width * 0.7).margin(20.0)
+    
+    emptyHolderView.isHidden = true
+  }
+  
+  @objc private func addButtonTap(sender: UIButton) {
+    viewModel.input.onAdd.accept(Void())
+  }
 }
 
 // MARK: - Animations
 
 extension TrainingViewController {
-    private func setupLoadingAnimationView() {
-        loadingAnimationView = AnimationView(name: "ProcessingCircle")
-        configDefaults(with: loadingAnimationView)
-    }
-    
-    private func setupCupAnimationView() {
-        cupAnimationView = AnimationView(name: "Cup")
-        configDefaults(with: cupAnimationView)
-    }
-    
-    private func configDefaults(with animationView: AnimationView) {
-        animationView.pin.height(200.0).width(200.0)
-        animationView.backgroundColor = Constants.whiteColor
-        animationView.contentMode = .scaleAspectFit
-        animationView.loopMode = .autoReverse
-        animationView.animationSpeed = 1.1
-        view.addSubview(animationView)
-        animationView.isHidden = true
-    }
+  private func setupCupAnimationView() {
+    cupAnimationView = AnimationView(name: "Cup")
+    configDefaults(with: cupAnimationView)
+  }
+  
+  private func configDefaults(with animationView: AnimationView) {
+    animationView.pin.height(200.0).width(200.0)
+    animationView.backgroundColor = Constants.whiteColor
+    animationView.contentMode = .scaleAspectFit
+    animationView.loopMode = .playOnce
+    animationView.animationSpeed = 1.1
+    view.addSubview(animationView)
+    animationView.isHidden = true
+  }
 }
 
 //MARK: - Constants
 
 private enum Constants {
-    static let title = "Training".localized
-    static let emptyLabelText = "Your dictionary is empty, add words and come back!".localized
-    static let titleAddButton = "Add".localized
-    
-    static let whiteColor = ColorScheme.white.color
-    static let blackColor = ColorScheme.black.color
-    static let blueColor = ColorScheme.fuchsiaBlue.color
-    
-    static let boldFont = UIFont.boldSystemFont(ofSize: 20.0)
+  static let title = "Training".localized
+  static let emptyLabelText = "Your dictionary is empty, add words and come back!".localized
+  static let titleAddButton = "Add".localized
+  
+  static let whiteColor = ColorScheme.white.color
+  static let blackColor = ColorScheme.black.color
+  static let blueColor = ColorScheme.fuchsiaBlue.color
+  
+  static let boldFont = UIFont.boldSystemFont(ofSize: 20.0)
 }
